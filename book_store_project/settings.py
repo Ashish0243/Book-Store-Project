632e432e0324eeda530337756ad3a0a697a6b2f9
Ashish0243 <ashishrajpal73@gmail.com>
2024-10-23 21:05:21 +0530

    customized email

--- book_store_project/settings.py
+++ book_store_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'crispy_bootstrap4',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'users',
@@ -158,5 +159,12 @@ ACCOUNT_USERNAME_REQUIRED=False
 ACCOUNT_AUTHENTICATION_METHOD='email'
 ACCOUNT_EMAIL_REQUIRED=True
 ACCOUNT_UNIQUE_EMAIL=True
+DEFAULT_FROM_EMAIL="admin@djangobookstore.com"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS=True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ 
